ed56b7474d9bfa932525eaf394d436007331267f
Arsienij Aniščanka <BatsilaBox3@gmail.com>
2019-09-20 13:51:12 +0300

    ISM-lab3

---
+++ ISM/Lab3/MGenerator.cs
@@ -0,0 +1,26 @@
+﻿namespace Lab3
+{
+    /// <summary>
+    /// MacLaren–Marsaglia generator for base random variable 
+    /// </summary>
+    public class MGenerator
+    {
+        private double _alpha;
+        private double _beta;
+        private double _m;
+
+        public MGenerator(int seed = 65539)
+        {
+            _alpha = seed;
+            _beta = 65539;
+            _m = 2147483648;
+        }
+
+        public double NextRand()
+        {
+            _alpha = (_alpha * _beta) % _m;
+
+            return _alpha / _m;
+        }
+    }
+}
---
+++ ISM/Lab3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Program
+    {
+        private const double A = 0;
+        private const double B = 9;
+
+        private delegate double Func(double x);
+
+        static double Function(double x)
+        {
+            return Math.Pow(Math.Cos(x), 2) / (1 + Math.Pow(x, 2));
+        }
+
+        static double SymmetryFunction(double x)
+        {
+            return (Function(x) + Function(A + B - x)) / 2;
+        }
+
+        static double DoMontecarlo(Func func, int samples = 10000)
+        {
+            var uniformGenerator = new UniformGenerator(A, B);
+            var ans = 0.0;
+
+            for (int i = 0; i < samples; ++i)
+            {
+                ans += func(uniformGenerator.NextRand());
+            }
+
+            return ans / samples * (B - A);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Function = {DoMontecarlo(Function)}");
+            Console.WriteLine($"Symmetry function = {DoMontecarlo(SymmetryFunction)}");
+
+            Console.ReadKey();
+        }
+    }
+}
