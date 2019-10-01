72922899de7b57107d7f0aa74497d355b448f267
Arsienij Aniščanka <BatsilaBox3@gmail.com>
2019-10-01 13:16:21 +0300

    CSDS-lab1 add vigenereEncryption

---
+++ CSDS/Lab1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ve = new VigenereEncryption();
+
+            var enTest = ve.Encrypt("Small test text", "Key");
+
+            Console.WriteLine(enTest);
+
+            var deTest = ve.Decrypt(enTest, "Key");
+
+            Console.WriteLine(deTest);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSDS/Lab1/VigenereEncryption.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    /// <summary>
+    /// The class that implements Vigenere encryption 
+    /// </summary>
+    public class VigenereEncryption
+    {
+        public const string ENG_ALPHABET = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+        public const string RUS_ALPHABET = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+
+        private readonly string _alphabet;
+
+        public VigenereEncryption(string alphabet = ENG_ALPHABET)
+        {
+            _alphabet = alphabet;
+        }
+
+        public string Encrypt(string text, string key) => DoVigenereCipher(text, key, true);
+
+        public string Decrypt(string text, string key) => DoVigenereCipher(text, key, false);
+
+        private string DoVigenereCipher(string text, string key, bool isEncrypt)
+        {
+            var gamma = RepeatKey(key, text.Length);
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < text.Length; ++i)
+            {
+                var textIndex = _alphabet.IndexOf(text[i]);
+                var gammaIndex = _alphabet.IndexOf(gamma[i]);
+
+                if (textIndex < 0)
+                {
+                    sb.Append(text[i].ToString());
+                }
+                else
+                {
+                    var index = (_alphabet.Length + textIndex + gammaIndex * 
+                        (isEncrypt ? 1 : -1)) % _alphabet.Length;
+                    sb.Append(_alphabet[index]);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        private string RepeatKey(string key, int length)
+        {
+            StringBuilder sb = new StringBuilder(key);
+
+            while (sb.Length < length)
+                sb.Append(key);
+
+            return sb.ToString();
+        }
+    }
+}
