955925175384279c3d4c84cab627d8512eefeb4e
Arsienij Aniščanka <BatsilaBox3@gmail.com>
2019-11-14 10:19:58 +0300

    FP add index

--- Temp/fp_db/SQL.sql
+++ Temp/fp_db/SQL.sql
@@ -1,3 +1,33 @@
+CREATE FUNCTION [h_d] 
+(
+	@x varbinary(200),
+	@y varbinary(200)
+)
+RETURNS int
+AS
+BEGIN
+	declare @distance int;
+
+		declare @i int;
+		
+		set @i = 0;
+		set @distance = 0;
+
+		WHILE @i < 200
+		begin
+			if not (SUBSTRING(@x, @i, 1) = SUBSTRING(@y, @i, 1))
+			begin
+				set @distance = @distance + 1;
+			end
+			set @i = @i + 1;
+		end
+		
+
+
+	return @distance
+END
+GO
+
 CREATE TABLE [dbo].[drug_like]
 (
 	[zinc_id] [varchar](255) NOT NULL,
@@ -11,4 +41,7 @@ CREATE TABLE [dbo].[drug_like]
 )
 ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
+CREATE NONCLUSTERED INDEX [IX_drug_like_fp] ON [drug_like]([fp]);
+
 INSERT INTO [dbo].[drug_like] VALUES('test', 'test', 0x0);
+
